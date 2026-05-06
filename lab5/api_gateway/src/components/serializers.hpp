3c1585b5cfba85970310a2b059d2658905255b58
Nifacy <mai.grishin.alexey@gmail.com>
2026-05-06 18:47:00 +0300

    migrate in generic implementation

--- lab5/api_gateway/src/components/serializers.hpp
+++ lab5/api_gateway/src/components/serializers.hpp
@@ -5,57 +5,37 @@
 #include <userver/formats/json.hpp>
 #include <userver/utils/boost_uuid4.hpp>
 
-namespace userver::formats::parse {
-
-inline boost::uuids::uuid Parse(const bson::Value& value,
-                                To<boost::uuids::uuid>) {
-  return utils::BoostUuidFromString(value.As<std::string>());
-}
-
-}  // namespace userver::formats::parse
-
-namespace userver::formats::serialize {
-
-inline bson::Value Serialize(const boost::uuids::uuid& value, To<bson::Value>) {
-  return bson::MakeDoc("value", utils::ToString(value))["value"];
-}
-
-}  // namespace userver::formats::serialize
-
 namespace boost::uuids {
 
-// TODO: migrate on generic implementation
-inline uuid Parse(const userver::formats::json::Value& value,
-                  userver::formats::parse::To<boost::uuids::uuid>) {
-  return userver::utils::BoostUuidFromString(value.As<std::string>());
+template <class Value>
+uuid Parse(const Value& data, userver::formats::parse::To<uuid>) {
+  return userver::utils::BoostUuidFromString(data.template As<std::string>());
 }
 
-inline userver::formats::json::Value Serialize(
-    const uuid& value,
-    userver::formats::serialize::To<userver::formats::json::Value>) {
-  return userver::formats::json::MakeObject(
-      "value", userver::utils::ToString(value))["value"];
+template <class Value>
+Value Serialize(const uuid& data, userver::formats::serialize::To<Value>) {
+  typename Value::Builder builder;
+  builder["value"] = userver::utils::ToString(data);
+  return builder.ExtractValue()["value"];
 }
 
 }  // namespace boost::uuids
 
 namespace std {
 
-template <typename A, typename B>
-pair<A, B> Parse(const userver::formats::json::Value& value,
+template <class Value, typename A, typename B>
+pair<A, B> Parse(const Value& value,
                  userver::formats::parse::To<std::pair<A, B>>) {
   value.CheckArray();
-  return {value[0].As<A>(), value[1].As<B>()};
+  return {value[0].template As<A>(), value[1].template As<B>()};
 }
 
-template <typename A, typename B>
-userver::formats::json::Value Serialize(
-    const pair<A, B>& value,
-    userver::formats::serialize::To<userver::formats::json::Value>) {
-  userver::formats::json::ValueBuilder builder;
+template <class Value, typename A, typename B>
+Value Serialize(const pair<A, B>& value,
+                userver::formats::serialize::To<Value>) {
+  typename Value::Builder builder;
   builder.PushBack(value.first);
   builder.PushBack(value.second);
-
   return builder.ExtractValue();
 }
 
